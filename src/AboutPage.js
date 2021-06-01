e6c321f8e9ece39b577aca338ab13f985cabcb32
Paul Schreiber <paulschreiber@gmail.com>
2021-06-01 18:02:48 -0400

    eslint code style fixes

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -7,8 +7,8 @@ function aboutPage () {
   return (
     <React.Fragment>
       <h2>{t('About.AdditionalDetails')}</h2>
-      <p dangerouslySetInnerHTML={{__html: t('About.Paragraph1', { interpolation: { escapeValue: false } })}} />
-      <p dangerouslySetInnerHTML={{__html: t('About.Paragraph2', { interpolation: { escapeValue: false } })}} />
+      <p dangerouslySetInnerHTML={{ __html: t('About.Paragraph1', { interpolation: { escapeValue: false } }) }} />
+      <p dangerouslySetInnerHTML={{ __html: t('About.Paragraph2', { interpolation: { escapeValue: false } }) }} />
     </React.Fragment>
   );
 }
