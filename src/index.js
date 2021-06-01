47b30befa23172e3b7954a283e42dcad6f1b57ed
Paul Schreiber <paulschreiber@gmail.com>
2021-06-01 16:50:18 -0400

    remove unused code in index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Main';
-import './i18n.js';
 import i18next from 'i18next';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './i18n.js';
 import 'bootstrap';
-
-import { config } from './config.js';
-//  USED FOR VISUAL COMPONENTS: COULD BE CHANGED TO MAKE SMALLER
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 require('dotenv').config();
 
-const url = config.url.API_URL;
-console.log(url);
 
-// initialize i18next (translation)
 i18next.init({
   interpolation: { escapeValue: false }
 });
 
-/* Renders the root component, wrapping in <Suspense> (prevents errors in i18n) and
-ApolloProvider (gives access to Apollo client features) */
 ReactDOM.render(
   <Suspense fallback={null}>
     <Main />
