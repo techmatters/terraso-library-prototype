5349f303cb483f844c5f2d01f9cb8a57b3991d4d
Paul Schreiber <paulschreiber@gmail.com>
2021-06-01 19:02:10 -0400

    use React.Frament instead of <> in home page

--- src/Homepage.js
+++ src/Homepage.js
@@ -5,10 +5,10 @@ function homePage () {
   // initializes useTranslation hook
   const { t } = useTranslation();
   return (
-    <>
+    <React.Fragment>
       <h2>{t('Home.ContentViewerPrototype')}</h2>
       <p>{t('Home.Paragraph')}</p>
-    </>
+    </React.Fragment>
   );
 }
 
